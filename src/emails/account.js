0313400a667dcef8e7298978ab6c4bdb7e748d15
AlexAlves5000 <alexalves5000@gmail.com>
2023-03-01 20:23:39 -0300

    Estudo do Jest

--- src/emails/account.js
+++ src/emails/account.js
@@ -10,12 +10,12 @@ const sendwellcomeEmail = (email, name) => {
         subject: 'Boas Vindas - Task Manager',
         text: 'Olá ' + name + ' seja bem vindo ao app Task Manager!'
     })
-    .then(() => {
-        console.log('Email enviado')
-    })
-    .catch((e) => {
-        console.error(e)
-    })
+        .then(() => {
+            console.log('Email enviado')
+        })
+        .catch((e) => {
+            console.error(e)
+        })
 }
 
 const sendcancelationEmail = (email, name) => {
@@ -25,32 +25,15 @@ const sendcancelationEmail = (email, name) => {
         subject: 'Sentiremos sua falta!',
         text: 'Olá ' + name + ', é uma pena que tenha tomado a decisão de cancelar a sua conta no Task Manager, mas ficaremos esperando o seu retorno!'
     })
-    .then(() => {
-        console.log('Email enviado')
-    })
-    .catch((e) => {
-        console.error(e)
-    })
+        .then(() => {
+            console.log('Email enviado')
+        })
+        .catch((e) => {
+            console.error(e)
+        })
 }
 
 module.exports = {
     sendwellcomeEmail,
     sendcancelationEmail
 }
-
-
-// const msg = {
-//     to: 'alexalves5000@gmail.com', // Change to your recipient
-//     from: 'alexalves5000@gmail.com', // Change to your verified sender
-//     subject: 'Aula 131',
-//     text: 'Este é um email de teste.',
-//     html: '<strong>Teste de envio de email</strong>',
-//   }
-
-// sgMail.send(msg)
-//     .then(() => {
-//         console.log('Email enviado')
-//     })
-//     .catch((e) => {
-//         console.error(e)
-//     })
